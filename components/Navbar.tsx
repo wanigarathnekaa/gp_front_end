0f09fe050044a5d21e7f47467456fce70e27e2b1
NethmiHasi <nethuhasi2001@gmail.com>
2024-07-01 10:45:00 +0700

    nav bar updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,24 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Navbar() {
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 flex justify-between px-20 py-2.5 items-center bg-white  ">
-      <img src="logo.png" alt="Logo" className="h-8" style={{ width: '120px', height: '40px' }}/>
+      <Image src="/Logo.png" alt="Logo" className="h-8"  width={120} height={40}/>
       <div className="flex items-center">
         
         <ul className="flex items-center space-x-7">
           
             <li>
-                <img src="settingsIcon.svg" alt="settingsIcon" className="h-8" style={{ width: '25px', height: '25px' }} />
+                <Image src="/settingsIcon.svg" alt="settingsIcon" className="h-8" width={25} height={25}  />
             </li>
 
           <li>
-            <img src="bell.svg" alt="bellIcon" className="h-8" style={{ width: '25px', height: '25px' }}/>
+            <Image src="/bell.svg" alt="bellIcon" className="h-8"   width={25} height={25} />
 
           </li>
           <li>
-            <img src="profilePic.png" alt="profilePicture" className="h-8" style={{ width: '35px', height: '35px' }}/>
+            <Image src="/profilePic.png" alt="profilePicture" className="h-8"  width={35} height={35}/>
           </li>
         </ul>
       </div>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Sidebar() {
   return (
     <div className="flex flex-col h-[92vh] bg-gray-50 w-64 p-3 shadow-lg fixed top-15">
       <div className="flex items-center space-x-4 p-2 mb-5 top-16 ml-4">
-        <img 
+        <Image
           className="h-12 w-12 rounded-full" 
-          src="profilePic.png" 
+          src="/profilePic.png" 
           alt="Profile Picture" 
           width={48} 
           height={48} 
