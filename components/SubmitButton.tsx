eca77a0dbbdc4e384f7448d1f3912f8f34fd9ae7
Thevindu Rathnayake <2021is079@stu.ucsc.cmb.ac.lk>
2024-07-11 18:50:15 +0530

    Created User details form

---
+++ components/SubmitButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SubmitButton() {
+  return (
+    <div>
+        <button className="text-white bg-[#4F46E5] hover:bg-[#3b31f8] hover:text-white font-bold py-2 px-4 rounded">Print</button>
+    </div>
+  )
+}
+
+export default SubmitButton
