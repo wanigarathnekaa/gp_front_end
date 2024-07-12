aadbe4e679c7a997bf9577559a5eca561292c381
NethmiHasi <nethuhasi2001@gmail.com>
2024-07-12 23:45:29 +0700

    components added

--- app/dashboard/users/view-registered/students/page.tsx
+++ app/dashboard/users/view-registered/students/page.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import { Sidebar, Navbar, Title, Table, Navigation } from '@/components';
-import Link from 'next/link';
+"use client";
+import React, { useState } from 'react'
+import { Sidebar, Navbar, Title, Table, Navigation, Dropdown, SearchBar } from '@/components';
+
 
 const usersData = [
   {
@@ -12,6 +13,7 @@ const usersData = [
     phone: '1234567890',
     fixedLine: '0987654321',
     year: '2nd Year',
+    type :"Postgraduate"
   },
   {
     userId: '2',
@@ -22,17 +24,7 @@ const usersData = [
     phone: '1234567890',
     fixedLine: '0987654321',
     year: '2nd Year',
-  },
-
-  {
-    userId: '2',
-    userName : "John Doe",
-    regNo: '2021CS001',
-    indexNo: 21345001,
-    email: 'user1@example.com',
-    phone: '1234567890',
-    fixedLine: '0987654321',
-    year: '2nd Year',
+    type:"Graduate",
   },
 
   {
@@ -44,11 +36,45 @@ const usersData = [
     phone: '1234567890',
     fixedLine: '0987654321',
     year: '2nd Year',
+    type: 'Undergraduate'
+  },
+
+  {
+    userId: '2',
+    userName : "John Doe",
+    regNo: '2021CS001',
+    indexNo: 21345001,
+    email: 'user1@example.com',
+    phone: '1234567890',
+    fixedLine: '0987654321',
+    year: '2nd Year',
+    type:"Undergraduate",
   },
   
 ];
 
 const StudentsData = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState("Undergraduates");
+
+  const handleSelect = (selectedOption : string) => {
+    setSelectedCategory(selectedOption);
+  };
+
+  const [users, setUsers] = useState(usersData);
+
+  const handleSearch = (searchTerm: string) => {
+    const filteredUsers = usersData.filter(user =>
+      user.userName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      user.regNo.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      user.userId.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      user.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      user.year.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      user.type.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setUsers(filteredUsers);
+  };
   
 
   return (
@@ -59,8 +85,24 @@ const StudentsData = () => {
       <div className=' mt-14 ml-64 flex flex-col min-h-screen bg-[#D6D6FF] p-4'>
       <Title text='Registered Users' />
       <Navigation/>
+
+      <div className='flex items-center justify-start mt-8 mb-4 ml-16 gap-14'>
+
+        <SearchBar onSearch={handleSearch}/>
+        <Dropdown 
+          options={['Undergraduates', 'Postgraduates', 'Graduates']}
+          onSelect={handleSelect}
+        />
+
+        <Dropdown
+          options={["1st Year", "2nd Year", "3rd Year", "4th Year"]}
+          onSelect={handleSelect}
+        />
+
+        
+      </div>
       
-      <Table users={usersData} type="student"/>
+      <Table users={users} type="student"/>
 
     </div>
     </div>
