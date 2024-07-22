18523c39f7560699d3a5f85bc16fd401a298908b
Sarani <saranihettiarachchi@gmail.com>
2024-07-23 01:01:49 +0530

    forms-view

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -32,11 +32,11 @@ function Sidebar() {
             
           </li>
           <li>
-            
-              <a className="flex items-center justify-center space-x-3 text-gray-700 p-2 rounded-md font-medium bg-[#e2e4e7] hover:bg-[#EEF2FF] hover:text-[#6366F1]">
+            <Link  href="/dashboard/forms"
+              className="flex items-center justify-center space-x-3 text-gray-700 p-2 rounded-md font-medium bg-[#e2e4e7] hover:bg-[#EEF2FF] hover:text-[#6366F1]">
                 Forms
-              </a>
-            
+              
+            </Link>
           </li>
           <li>
             
