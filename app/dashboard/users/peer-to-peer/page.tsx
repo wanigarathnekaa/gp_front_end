259fcef0860c4191991d22dea0eec4e6e6153ab9
NethmiHasi <nethuhasi2001@gmail.com>
2024-07-23 14:17:39 +0700

    peer-to-peer

--- app/dashboard/users/peer-to-peer/page.tsx
+++ app/dashboard/users/peer-to-peer/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react'
-import { Sidebar, Navbar, Title, Table, Card, Navigation } from   '@/components';
+import { Sidebar, Navbar, Title, Table, Card, Navigation, ReviewTable } from   '@/components';
 import { FaUser, FaUsers} from 'react-icons/fa'; 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -11,7 +11,15 @@ const links =[
         label : 'Lecturers'
     }
 
-]
+];
+
+const review = [
+    { id: 1, reviewer: 'John Doe', reviewee: 'Jane Smith', reviewDate: '2024-07-20' },
+    { id: 2, reviewer: 'Alice Brown', reviewee: 'Bob Johnson', reviewDate: '2024-07-21' },
+    { id: 3, reviewer: 'Alice Brown', reviewee: 'Bob Johnson', reviewDate: '2024-07-21' },
+    { id: 4, reviewer: 'Alice Brown', reviewee: 'Bob Johnson', reviewDate: '2024-07-21' },
+    
+];
 
 const PeerReviews = () => {
     const pathname = usePathname();
@@ -20,8 +28,8 @@ const PeerReviews = () => {
             <Navbar />
             <Sidebar />
 
-            <div className=' mt-14 ml-64  min-h-screen bg-[#D6D6FF] p-4'>
-                <Title text='Peer-to-peer reviews' />
+            <div className=' mt-12 ml-64  min-h-screen bg-[#D6D6FF] p-4'>
+                <Title text='Peer-to-peer review' />
 
                 <>
                     <Navigation links={links} pathname={pathname} />
@@ -29,12 +37,23 @@ const PeerReviews = () => {
 
                 <div className='flex flex-row gap-10 mt-8 ml-10 '>
 
-                    
+                    <Link href='/dashboard/users/peer-to-peer/Assign-new'>
+                        <Card 
+                            title='Assign New Peer Review'
+                            description='Assign a new peer review'
+                            icon={FaUser}
+                        />
+                    </Link>
 
                 </div>
 
-                <div className='ml-14 mt-10'>
-                    <h4 className='text-xl font-semibold'>Enrollment history</h4>
+                <div className='mt-10'>
+                    <h4 className='text-xl font-semibold ml-14 '>Review history</h4>
+
+                    <div className='ml-5 mt-4'>
+                        <ReviewTable users={review} />
+
+                    </div>
                 </div>
 
             </div>
