2431f101b65c7d09f1e5bd46a52c0d50caaecb7a
NethmiHasi <nethuhasi2001@gmail.com>
2024-07-25 23:43:55 +0700

    updated userinfo form

--- app/dashboard/users/UserDetails/UpdateUserDetails/page.tsx
+++ app/dashboard/users/UserDetails/UpdateUserDetails/page.tsx
@@ -2,6 +2,16 @@ import React from 'react'
 import Navbar from '@/components/Navbar';
 import UserInfoForm from '@/components/UserInfoForm';
 
+const studentFields = [
+  { label: 'Full Name', placeholder: 'Placeholder', disabled: false, colSpan: 2 },
+  { label: 'Registration no', placeholder: 'Placeholder', disabled: false },
+  { label: 'Index no', placeholder: 'Placeholder', disabled: false },
+  { label: 'Email', placeholder: 'Placeholder', disabled: false, colSpan: 2 },
+  { label: 'NIC', placeholder: 'Placeholder', disabled: false },
+  { label: 'Mobile no', placeholder: 'Placeholder', disabled: false },
+  { label: 'Year of studying', placeholder: 'Placeholder', disabled: false },
+  { label: 'Type', placeholder: 'Placeholder', disabled: false },
+];
 
 
 function page() {
@@ -12,7 +22,7 @@ function page() {
         
         <div className="flex items-center justify-center h-screen">
           
-            <UserInfoForm mode = "edit" />
+            <UserInfoForm mode = "edit" fields={studentFields} />
         </div>
         
     </div>
