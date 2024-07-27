ca6b5803a9c10b1a35490c2217e3f75f28654294
Thevindu Rathnayake <2021is079@stu.ucsc.cmb.ac.lk>
2024-07-27 16:57:42 +0530

    created card component for examDep dashboard

--- app/ExaminationDep/dashboard/page.tsx
+++ app/ExaminationDep/dashboard/page.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
-import { Navbar } from '@/components';
+import { Navbar, } from '@/components';
 import ExamSidebar from '@/components/ExamSidebar';
-
+import CloakCountCard from '@/components/CloakCountCard';
 
 
 function page() {
   return (
-    <div>
+    <div className='bg-[#D6D6FF]'>
         <Navbar />
         <ExamSidebar />
+
+        <h1 className='text-3xl font-bold pl-96 pt-24'>Examination Department Dashboard</h1>
+
+        <div className='flex flex-row justify-center pl-64 mt-16'>
+
+          <CloakCountCard 
+            title='Total Cloaks' 
+            countSmall={100}
+            countMedium={200}
+            countLarge={300}
+            description='Total number of cloaks in the system' 
+            wide />
+
+          <CloakCountCard 
+            title='Cloaks Issued' 
+            countSmall={150}
+            countMedium={250}
+            countLarge={350}
+            description='Total number of cloaks in the system' 
+            wide />
+
+          <CloakCountCard 
+            title='Cloaks Returned' 
+            countSmall={130}
+            countMedium={230}
+            countLarge={330}
+            description='Total number of cloaks in the system' 
+            wide />
+        </div>
+        
         
         
     </div>
