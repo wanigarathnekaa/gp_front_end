df2941c0cd92cf44a9d401e729c5ee6d9fe45541
NethmiHasi <nethuhasi2001@gmail.com>
2024-07-27 15:31:54 +0700

    forms dashboard

---
+++ app/dashboard/forms/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import { Navbar, Sidebar } from '@/components'
+import React from 'react'
+
+const FormsDashboard = () => {
+  return (
+    <div className='w-full'>
+        <Navbar/>
+        <Sidebar/>
+
+        <div className='mt-12 ml-64 bg-bg-[#D6D6FF]'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default FormsDashboard
