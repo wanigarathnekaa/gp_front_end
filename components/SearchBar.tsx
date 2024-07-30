dcefd0def02d0bce50eedfe64c2225b2b75bb94b
NethmiHasi <nethuhasi2001@gmail.com>
2024-07-30 18:29:03 +0700

    search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -14,7 +14,7 @@ const SearchBar = ({onSearch}:SearchBarProps) => {
         onSearch(searchTerm);
     }
     return (
-        <div className="flex">
+        <div className="flex ">
             <input
                 type="text"
                 className="border border-gray-300 rounded-l-full p-2 h-10 w-80 focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -24,7 +24,7 @@ const SearchBar = ({onSearch}:SearchBarProps) => {
             />
 
             <button
-                className="bg-blue-600 text-white p-2 h-10 rounded-r-full focus:outline-none focus:ring-2 focus:ring-blue-500 flex items-center justify-center "
+                className="bg-blue-600 text-white p-2 h-10  rounded-r-full focus:outline-none focus:ring-2 focus:ring-blue-500 flex items-center justify-center "
                 onClick={handleSearch}
                 aria-label="Search"
             > 
