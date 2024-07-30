65ff5fb5778fbb5480eb3004ed01fba8a59214c2
Sarani <saranihettiarachchi@gmail.com>
2024-07-30 19:01:44 +0530

    Lecturer Dashboard

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -69,7 +69,7 @@ const Page: React.FC = () => {
                   onChange={handleRegNumberChange}
                   placeholder="2021/CS/001"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 pl-4 pr-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 pl-4 pr-4 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -92,11 +92,10 @@ const Page: React.FC = () => {
               </div>
             </div>
 
-            <div>
+            <div className="py-2">
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                 Login
               </button>
             </div>
