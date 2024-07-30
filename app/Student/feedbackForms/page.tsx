23cfe2e1aaa662641a1c8cd059bbe90230037467
NethmiHasi <nethuhasi2001@gmail.com>
2024-07-30 18:27:03 +0700

    updated

---
+++ app/Student/feedbackForms/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StudentNavbar, FeedbackForm } from '@/components'
+
+const page = () => {
+  return (
+
+    <div className='w-full'>
+    
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+            <FeedbackForm />
+        </div>
+    </div>
+    );
+  
+}
+
+export default page
