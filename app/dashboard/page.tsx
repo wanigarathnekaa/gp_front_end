5d5e86fed9fa95650ce1ac2c43253f35dab666a4
Sarani <saranihettiarachchi@gmail.com>
2024-08-11 03:18:10 +0530

    Enroll new page finalized

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -34,54 +34,72 @@ const Dashboard = () => {
                             title="View registered users" 
                             description='Assign user privileges to any user'
                             icon={FaUsers}
-                            
+                            wide={true}
                         />
 
                     
                 </Link>
 
-            <Link href="/dashboard/users/enroll-new/students">
-              <Card
-                title="Enroll new users"
-                description="View existing user privileges and roles"
-                icon={IoMdAddCircle}
-              />
-            </Link>
+                <Link href="/dashboard/users/enroll-new/students">
+                    
+                        <Card 
+                            title="Enroll new users" 
+                            description='View existing user privileges and roles'
+                            icon={IoMdAddCircle}
+                            wide={true}
+                        />
 
-            <Link href="/dashboard/users/create-new">
-              <Card
-                title="Create new user role"
-                description="View existing user privileges and roles"
-                icon={HiUserAdd}
-              />
-            </Link>
+                    
+                </Link>
 
-            <Link href="/dashboard/users/peer-to-peer">
-              <Card
-                title="Peer-to-peer review"
-                description="Assign peer-to-peer reviewers"
-                icon={AiFillMessage}
-              />
-            </Link>
+                <Link href="/dashboard/users/create-new">
+                    
+                        <Card 
+                            title="Create new user role" 
+                            description='View existing user privileges and roles'
+                            icon={HiUserAdd}
+                            wide={true}
+                        />
 
-            <Link href="/dashboard/users/view-registered/students/ranking">
-              <Card
-                title="Student ranking"
-                description="View students ranking"
-                icon={FaRankingStar}
-              />
-            </Link>
+                    
+                </Link>
+
+                <Link href="/dashboard/users/peer-to-peer">
+                    
+                        <Card 
+                            title="Peer-to-peer review" 
+                            description='Assign peer-to-peer reviewers'
+                            icon={AiFillMessage}
+                            wide={true}
+                        />
+
+                   
+                </Link>
+
+                <Link href="/dashboard/users/view-registered/students/ranking">
+                    
+                        <Card 
+                            title="Student ranking" 
+                            description='View students ranking'
+                            icon={FaRankingStar}
+                            wide={true}
+                        />
+
+                   
+                </Link>
+                
+            </div>
           </div>
-        </div>
-        <div className="w-1/4 p-4 bg-white shadow-md">
-          <Calendar />
-          <TaskList
-            tasks={["Task 1", "Task 2", "Task 3", "Task 4"]}
-            icon={FaRegClock}
-          />
+          <div className="w-1/4 p-4 bg-white shadow-md">
+            <Calendar />
+            <TaskList 
+              tasks={['Task 1', 'Task 2', 'Task 3', 'Task 4']}
+              icon = {FaRegClock} 
+            />
+          </div>
+          
         </div>
       </div>
-    </div>
   );
 };
 
