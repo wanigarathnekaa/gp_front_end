41cc25f23b3895f93077ffed3bbee17f0e8fde41
NethmiHasi <nethuhasi2001@gmail.com>
2024-08-22 13:56:13 +0700

    cloak

---
+++ app/ExaminationDep/Cloak/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { CloakBarChart } from '@/components/index'
+
+const Cloak = () => {
+  return (
+    <div>
+        <h2 className="text-2xl font-semibold text-gray-600 ml-3 px-6">Cloak Distribution Chart</h2>
+        <CloakBarChart />
+
+    </div>
+  )
+}
+
+export default Cloak
