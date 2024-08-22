7df21c87305daf5894b7c393300f2dba0de60d40
NethmiHasi <nethuhasi2001@gmail.com>
2024-08-22 14:51:23 +0700

    history update

--- app/ExaminationDep/Students/history/page.tsx
+++ app/ExaminationDep/Students/history/page.tsx
@@ -1,9 +1,119 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
+import { Navbar, ExamSidebar, Title, Navigation,  SearchBar, ExaminationTable } from '@/components/index'
+import { usePathname } from 'next/navigation';
+
+
+const links=[
+    {
+        href: '/ExaminationDep/Students/details',
+        label: 'home'
+    
+      },
+    
+      
+      {
+        href: '/ExaminationDep/Students/history',
+        label: 'History'
+      },
+];
+
+const usersData = [
+    {
+      userId: "1",
+      userName : "John Doe",
+      regNo: '2021CS100',
+      indexNo: '2100798',
+      size: 'Medium',
+      phone: '0724646186',
+      IssuedDate:'2023-05-01',
+      ReturnedDate:'2023-08-01',
+      status: 'Returned'
+    },
+    {
+      userId: '2',
+      userName : "Ann Fernando",
+      regNo: '2021CS001',
+      indexNo: '21345001',
+      size: 'Large',
+      phone: '0762341566',
+      IssuedDate:'2023-05-02',
+      ReturnedDate:'2023-10-02',
+      status: 'Returned'
+    },
+  
+    {
+      userId: '3',
+      userName : "Alex Silva",
+      regNo: '2021CS002',
+      indexNo: '21385001',
+      phone: '0765456789',
+      size: 'Small',
+      IssuedDate:'2024-05-08',
+      ReturnedDate:'2023-10-02',
+      status: 'Returned'
+    },
+  
+    {
+      userId: '4',
+      userName : 'James Perera',
+      regNo: '2020CS006',
+      indexNo: '20345001',
+      phone: '0743217890',
+      size: 'Medium',
+      IssuedDate:'2022-05-01',
+      ReturnedDate:'2022-08-05',
+      status: 'Returned'
+    },
+  ];
 
 const History = () => {
-  return (
-    <div>History</div>
-  )
+    const pathname = usePathname();
+    // console.log("pathname",pathname);
+
+    const [users, setUsers] = useState(usersData);
+
+    const handleSearch =(searchText : string) => {
+        const filteredUsers = usersData.filter(user =>
+            user.userId.toLowerCase().includes(searchText.toLowerCase()) ||
+            user.userName.toLowerCase().includes(searchText.toLowerCase()) ||
+            user.regNo.toLowerCase().includes(searchText.toLowerCase()) ||
+            user.indexNo.toLowerCase().includes(searchText.toLowerCase()) ||
+            user.size.toLowerCase().includes(searchText.toLowerCase()) ||
+            user.status.toLowerCase().includes(searchText.toLowerCase())||
+            user.IssuedDate.toLowerCase().includes(searchText.toLowerCase()) ||
+            user.ReturnedDate.toLowerCase().includes(searchText.toLowerCase())
+            
+        );
+    
+        setUsers(filteredUsers);
+    };
+
+    return (
+        <div className='w-full'>
+            <Navbar/>
+            <ExamSidebar/>
+
+            <div className='mt-12 ml-72 md:ml-64 lg:ml-72 bg-[#EEF2FF] flex flex-col min-h-screen'>
+                <div className='ml-3 mt-8'>
+                    <Title text='Student History'/>
+                </div>
+                
+                <Navigation links={links} pathname={pathname}/>
+
+                <div className="flex items-center mb-4 mt-4 ml-3 p-6 ">
+                    <div className="flex space-x-4">
+                        <SearchBar onSearch={handleSearch} />
+                    </div>
+                </div>
+
+                <div className='ml-3 px-6 mt-2'>
+                    <ExaminationTable users={users}/>
+                </div>
+            </div>
+        </div>
+    
+    )
 }
 
 export default History
