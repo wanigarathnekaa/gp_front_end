a0131c8c84f663e7831f074abc8adfd246511adc
Thevindu Rathnayake <2021is079@stu.ucsc.cmb.ac.lk>
2024-08-28 00:02:28 +0530

    popup for edit profile

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,86 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+"use client";
+
+import React, { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import ReactDOM from 'react-dom';
+import { CancelButton, DeleteButton, SubmitButton } from '@/components/index';
+import UserInfoForm from '@/components/UserInfoForm';
+
+
+function Modal({ isOpen, onClose }: { isOpen: boolean, onClose: () => void }) {
+  if (!isOpen) return null;
+
+  return ReactDOM.createPortal(
+    <div className="fixed inset-0 flex items-center justify-center z-[100] bg-black bg-opacity-50">
+      <div className="bg-white p-8 rounded-md space-y-4">
+        <h2 className="font-bold text-xl text-gray-600 mb-8">Edit Profile</h2>
+        <form className="space-y-4">
+          <div>
+            <label htmlFor="name" className="font-bold text-gray-500">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-lg"
+              placeholder="Enter your name"
+            />
+          </div>
+          <div>
+            <label htmlFor="email" className="font-bold text-gray-500">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-lg"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div>
+            <label htmlFor="address" className="font-bold text-gray-500">
+              Address
+            </label>
+            <textarea
+              id="address"
+              name="address"
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-lg"
+              placeholder="Enter your address"
+              rows={2}
+            />
+          </div>
+          <div className="flex justify-end space-x-3">
+            <button
+              type="button"
+              onClick={onClose}
+              className='text-gray-600 bg-gray-200 hover:bg-gray-800 hover:text-white font-bold py-2 px-4 rounded'
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="text-white bg-[#312e81ef] hover:bg-[#4e46e5b5] hover:text-white font-bold py-1.5 px-4 rounded-full cursor-pointer"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>,
+    document.body
+  );
+}
 
 function Sidebar() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <div className="flex flex-col h-[95vh] bg-[#312e81] w-72 p-3 fixed top-12">
       <div className="flex items-center space-x-6 pt-10 mb-5 top-20 justify-center">
@@ -15,53 +93,44 @@ function Sidebar() {
         />
         <div>
           <h4 className="font-semibold text-lg text-white">L.A Lakshani</h4>
-          <h6 className="text-[#d6e5ff] hover:underline cursor-pointer">Edit Profile</h6>
+          <h6 className="text-[#d6e5ff] hover:underline cursor-pointer" onClick={toggleModal}>
+            Edit Profile
+          </h6>
         </div>
       </div>
       <nav className="flex-1">
         <ul className="space-y-3 bg-[#312e81] mt-5 justify-center px-3">
-
           <li>
-
-            <Link  href="/dashboard"
-            
-               className="flex items-center justify-center space-x-3 text-gray-600 p-2 rounded-md font-medium bg-white hover:bg-[#EEF2FF] hover:text-[#706ee4] cursor-pointer transform transition-transform duration-300 hover:scale-105">
-                  Users
-              
-
-            </Link>
-            
-          </li>
-          <li>
-            <Link  href="/dashboard/forms"
-              className="flex items-center justify-center space-x-3 text-gray-600 p-2 rounded-md font-medium bg-white hover:bg-[#EEF2FF] hover:text-[#706ee4] cursor-pointer transform transition-transform duration-300 hover:scale-105">
-                Forms
-              
+            <Link href="/dashboard" className="flex items-center justify-center space-x-3 text-gray-600 p-2 rounded-md font-medium bg-white hover:bg-[#EEF2FF] hover:text-[#706ee4] cursor-pointer transform transition-transform duration-300 hover:scale-105">
+              Users
             </Link>
           </li>
           <li>
-            <Link href="/dashboard/privileges"
-              className="flex items-center justify-center space-x-3 text-gray-600 p-2 rounded-md font-medium bg-white hover:bg-[#EEF2FF] hover:text-[#706ee4] cursor-pointer transform transition-transform duration-300 hover:scale-105">
-                Privileges
-
+            <Link href="/dashboard/forms" className="flex items-center justify-center space-x-3 text-gray-600 p-2 rounded-md font-medium bg-white hover:bg-[#EEF2FF] hover:text-[#706ee4] cursor-pointer transform transition-transform duration-300 hover:scale-105">
+              Forms
             </Link>
-              
-            
           </li>
           <li>
-            
-              <a className="flex items-center justify-center space-x-3 text-gray-600 p-2 rounded-md font-medium bg-white hover:bg-[#EEF2FF] hover:text-[#706ee4] cursor-pointer transform transition-transform duration-300 hover:scale-105">
-                Analysis
-              </a>
-            
+            <Link href="/dashboard/privileges" className="flex items-center justify-center space-x-3 text-gray-600 p-2 rounded-md font-medium bg-white hover:bg-[#EEF2FF] hover:text-[#706ee4] cursor-pointer transform transition-transform duration-300 hover:scale-105">
+              Privileges
+            </Link>
+          </li>
+          <li>
+            <a className="flex items-center justify-center space-x-3 text-gray-600 p-2 rounded-md font-medium bg-white hover:bg-[#EEF2FF] hover:text-[#706ee4] cursor-pointer transform transition-transform duration-300 hover:scale-105">
+              Analysis
+            </a>
           </li>
         </ul>
       </nav>
       <div className="flex justify-center pb-5 px-3">
-        <button className="block w-full items-center justify-center space-x-3 text-gray-600 p-2 rounded-md font-medium bg-white hover:bg-[#EEF2FF] hover:text-[#706ee4] cursor-pointer transform transition-transform duration-300 hover:scale-105">Log Out</button>
+        <button className="block w-full items-center justify-center space-x-3 text-gray-600 p-2 rounded-md font-medium bg-white hover:bg-[#EEF2FF] hover:text-[#706ee4] cursor-pointer transform transition-transform duration-300 hover:scale-105">
+          Log Out
+        </button>
       </div>
+
+      <Modal isOpen={isModalOpen} onClose={toggleModal} />
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
