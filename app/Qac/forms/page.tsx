109022c655c16f6b6b272f6528ce6177b480085d
wanigarathnekaa <kavisha.abhishake@gmail.com>
2024-11-29 00:00:31 +0530

    Course Part Done

--- app/Qac/forms/page.tsx
+++ app/Qac/forms/page.tsx
@@ -67,7 +67,7 @@ export default function Home() {
       {/* <Separator className="my-6" /> */}
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <CreateFormBtn template={{ template: false }} />
+        <CreateFormBtn template={false} />
         <Suspense
           fallback={[1, 2, 3, 4].map((ele) => (
             <FormCardSkeleton key={ele} />
