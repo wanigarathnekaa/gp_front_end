5939925c712f0918634a5be364f6239d1755aa4e
NethmiHasi <nethuhasi2001@gmail.com>
2024-12-02 14:35:28 +0700

    adding and updating cloak count

--- actions/Cloak.ts
+++ actions/Cloak.ts
@@ -4,14 +4,24 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL;
 console.log("API URL:", API_URL);
 
 interface CloakCounts{
+    name: string;
     smallCount: number;
     mediumCount: number;
     largeCount: number;
 }
 
-export const addCloak = async(cloakCounts: CloakCounts) => {
+export const getCloakCount = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/api/cloaks/current`);
+      return response.data;
+    } catch (error) {
+      throw new Error("Failed to fetch cloak counts");
+    }
+  };
+
+export const addOrUpdateCloak = async(cloakCounts: CloakCounts) => {
     try{
-        const response = await axios.post(`${API_URL}/api/cloaks/add`, cloakCounts);
+        const response = await axios.post(`${API_URL}/api/cloaks/save`, cloakCounts);
         return response.data;
     }catch(error){
         throw new Error("Failed to add cloak counts");
