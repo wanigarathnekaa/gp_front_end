e3dd2a26dd975ff665317e8a423bebd98437a7b1
Naveesha Lakshan <lakshannaveesha10@gmail.com>
2024-12-02 21:15:23 +0530

    store roleName in session

--- context/AuthProvider.tsx
+++ context/AuthProvider.tsx
@@ -1,47 +1,48 @@
 "use client";
-import { createContext, useContext, useState, ReactNode, useEffect } from "react";
+import { createContext, useContext, useState, useEffect, ReactNode } from "react";
 
 interface AuthContextType {
   decodedToken: any;
-  setToken: (token: string) => void;
+  roleName: string | null;
+  setToken: (token: string, roleName: string) => void;
 }
 
-function parseJwt(token:string) {
-    if (!token) { return; }
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace('-', '+').replace('_', '/');
-    return JSON.parse(window.atob(base64));
-  }
+function parseJwt(token: string) {
+  if (!token) return;
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace("-", "+").replace("_", "/");
+  return JSON.parse(window.atob(base64));
+}
+
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [decodedToken, setDecodedToken] = useState(null);
+  const [roleName, setRoleName] = useState<string | null>(null);
 
-  // Load token from sessionStorage on app initialization
+  // Load token and role from sessionStorage on app initialization
   useEffect(() => {
-    const token = sessionStorage.getItem("user");
-    if (token) {
+    const user = sessionStorage.getItem("user");
+    if (user) {
       try {
-        const decoded = JSON.parse(token);
-        setDecodedToken(decoded);
+        const parsedUser = JSON.parse(user);
+        setDecodedToken(parsedUser.token);
+        setRoleName(parsedUser.roleName);
       } catch (error) {
-        console.error("Error decoding stored token:", error);
+        console.error("Error decoding stored user data:", error);
       }
     }
   }, []);
 
-
-  const setToken = (token: string) => {
-    let obj=parseJwt(token);
-
-    sessionStorage.setItem("user", JSON.stringify(obj));
-    const decoded = obj;
+  const setToken = (token: string, role: string) => {
+    const obj = parseJwt(token);
+    sessionStorage.setItem("user", JSON.stringify({ token: obj, roleName: role }));
     setDecodedToken(obj);
-    console.log("decoded",decoded);
+    setRoleName(role);
   };
 
   return (
-    <AuthContext.Provider value={{ decodedToken, setToken }}>
+    <AuthContext.Provider value={{ decodedToken, roleName, setToken }}>
       {children}
     </AuthContext.Provider>
   );
