accba35102871225cde190f2dec157a522052b28
wanigarathnekaa <kavisha.abhishake@gmail.com>
2024-12-02 21:53:05 +0530

    Added Changes to Student Dashboard

--- actions/course.ts
+++ actions/course.ts
@@ -3,9 +3,10 @@ import axios from 'axios';
 // Define the API URL from environment variables
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-interface courseRequest {
+interface CourseRequest {
     regNumber: string;
-    nic: string;
+    year: string;
+    semester: string;
   }
 
 
@@ -22,15 +23,13 @@ export const AllCourses = async () => {
   
 };
 
-export const getFilledCourses = async () => {
+
+export const getFilledCourses = async (courseRequest: CourseRequest) => {
     try {
-        // Make a GET request to fetch all courses
-        const response = await axios.get(`${API_URL}/course/allCourses`);
-        // Return the response data
-        return response.data;
+      const response = await axios.post(`${API_URL}/course/FilledCourse`, courseRequest);
+      return response.data;
     } catch (error) {
-        // Throw a descriptive error if the request fails
-        throw new Error('Failed to fetch courses: ' + (error as Error).message);
+      // Throw a descriptive error if the request fails
+      throw new Error('Failed to fetch courses: ' + (error as Error).message);
     }
-  
-};
+  };
