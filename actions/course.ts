d0e3639456c6c2e37271cbb15e4d06e217116c36
wanigarathnekaa <kavisha.abhishake@gmail.com>
2024-12-02 21:13:20 +0530

    Student-Login-Small-Change

--- actions/course.ts
+++ actions/course.ts
@@ -3,8 +3,26 @@ import axios from 'axios';
 // Define the API URL from environment variables
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
+interface courseRequest {
+    regNumber: string;
+    nic: string;
+  }
+
 
 export const AllCourses = async () => {
+    try {
+        // Make a GET request to fetch all courses
+        const response = await axios.get(`${API_URL}/course/allCourses`);
+        // Return the response data
+        return response.data;
+    } catch (error) {
+        // Throw a descriptive error if the request fails
+        throw new Error('Failed to fetch courses: ' + (error as Error).message);
+    }
+  
+};
+
+export const getFilledCourses = async () => {
     try {
         // Make a GET request to fetch all courses
         const response = await axios.get(`${API_URL}/course/allCourses`);
