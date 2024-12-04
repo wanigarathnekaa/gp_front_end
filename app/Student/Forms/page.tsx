82c0234ae619ae7a4db3411c289096a64fa5298f
Sarani <saranihettiarachchi@gmail.com>
2024-12-04 05:58:58 +0530

    student changes 2

--- app/Student/Forms/page.tsx
+++ app/Student/Forms/page.tsx
@@ -6,12 +6,18 @@ const forms = () => {
   return (
     <div className="w-full">
       <StudentNavbar />
-      <div className="flex flex-col bg-blue-50 justify-start items-center min-h-screen px-20 py-10">
-        {/* <div className="mt-12">
+      <div className="flex flex-col bg-blue-50 justify-start min-h-screen px-20 py-10">
+        <div className="mt-12 flex flex-col items-center w-full">
+          {/* Title aligned to the left */}
+          <div className="w-full text-left ml-72 mt-5">
             <Title text="Student Forms" />
-            </div> */}
+          </div>
 
+          {/* Table centered */}
+          <div className="w-full flex justify-center -mt-2">
             <FormListTable />
+          </div>
+        </div>
       </div>
     </div>
   );
